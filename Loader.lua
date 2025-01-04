2ee9e9a22ecdedf103797280c4fcef4efeec438e
Edwn <88218388+EdwinS7@users.noreply.github.com>
2025-01-03 21:34:25 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,5 @@
+-- This note is here because github is being retarded and wouldn't update the raw file!
+
 local LoadScript = function(source_code, whitelist_key)
     whitelist_key = whitelist_key or "nono dont touch me there, thats thats, my nono square!!"
 
